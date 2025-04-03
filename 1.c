826e15eed4304c95f0481c718a7a388881db8c4a
max-s-m <max.m.semenov@gmail.com>
2025-04-03 15:29:46 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -4,8 +4,8 @@
 #include <math.h>
 
 int main(){
-    printf("time_t bits (1 bit +- sign): %ld\n", sizeof(time_t) * 8);
+    printf("time_t bits (1 bit +- sign): %ld\n\n", sizeof(time_t) * 8);
     time_t time1 = pow(2, 63) - 1;
-    printf("Max time_t is 2^63: %ld\n", time1);
-    printf("++time1 result: %ld\nNegative value - OVERFLOWED\n", ++time1);
+    printf("Max time_t is 2^63 - 1: %ld\n\n", time1);
+    printf("++time1 result: %ld\nNegative value mrans 2^63 is max time, because it OVERFLOWED when ++time1\n", ++time1);
 }
