b33d7ed065411f208973bcfee5f58f5db7666776
max-s-m <max.m.semenov@gmail.com>
2025-04-03 15:20:48 +0300

    Add files via upload

---
+++ 2-2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int arr[1000];
+
+int main(){
+    printf("hello world\n");
+}
---
+++ 2-3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int arr[1000] = {1};
+
+int main(){
+    printf("hello world\n");
+}
